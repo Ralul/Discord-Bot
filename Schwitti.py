d091e413d6b5063604389ca6e8eeeff6e085e8fb
Ralul <74320658+Ralul@users.noreply.github.com>
2021-03-02 18:46:33 +0100

    Update Schwitti.py

--- Schwitti.py
+++ Schwitti.py
@@ -14,10 +14,6 @@ def countdown(t):
         time.sleep(1)
         t -= 1
 
-
-
-
-
 Bananenbrot = str("ODEyMTExMzM3MTIyODI0MjEy.YC7_nw.0psLbsWOrjMEOHRM4rbLexCsyR8_LOL")
 Currywurst = Bananenbrot[:-4]
 
@@ -36,12 +32,14 @@ class MyClinet(discord.Client):
             # Wenn Nachricht gepostet wird, dann drucke ich sie aus
             print("Da hat jemand etwas geschrieben" + " \ " + str(message.author) + " \ " + str(message.content))
 
-            await message.channel.send("Lets go")
+            await message.channel.send("Lets go!, The Final Countdown has Started!")
 
-            daily = 1
+            daily = 0
             hourly = 1
-            minutely = 2
-            secondary = 30
+            minutely = 0
+            secondary = 5
+
+            message_hourly = hourly
 
             legendsecondary = ((daily * 86400)+(hourly * 3600)+(minutely * 60)+(secondary * 1))
 
@@ -52,6 +50,8 @@ class MyClinet(discord.Client):
                 countdown(1)
                 legendsecondary = legendsecondary - 1
 
+                #Time OUtput in Day Hour Minutest Second
+
                 a_daily = legendsecondary // 86400
                 rest = legendsecondary % 86400
                 a_hourly = rest // 3600
@@ -60,23 +60,18 @@ class MyClinet(discord.Client):
                 rest = rest % 60
                 a_secondary = rest
 
-
                 print (a_daily,"¦",a_hourly,"¦",a_minutely,"¦",a_secondary)
 
+                #Hourly message
 
+                if a_hourly != message_hourly:
+                    message_hourly = a_hourly
+                    await message.channel.send ("finally only", "left")
 
-
-
-
-
-
-
-
-
-
-
-
-
+                if (a_secondary + a_minutely + a_hourly + a_daily == 0):
+                    message_hourly = a_hourly
+                    await message.channel.send("end of all")
+                    print ("null")
 
 
 
